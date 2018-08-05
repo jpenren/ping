53679e0c69969215eb70b8740fb5ff1b1b33b9c1
javi <javi@192.168.1.9>
2018-08-05 10:52:46 +0200

    Date format

--- src/main/java/ping/PingServlet.java
+++ src/main/java/ping/PingServlet.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,6 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 public class PingServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
+    private static final String ISO_8601 = "yyyy-MM-dd'T'HH:mm'Z'";
     private static final String APPLICATION_JSON = "application/json";
     private static final String JSON_TEMPLATE = "{\"hostname\":\"%s\", \"startup\":%d}";
     private final Data data;
@@ -32,12 +37,14 @@ public class PingServlet extends HttpServlet {
         final PrintWriter writer = resp.getWriter();
         if(supportsJson(req)) {
             resp.setContentType(APPLICATION_JSON);
-            final String json = String.format(JSON_TEMPLATE, data.hostname, data.startup);
+            DateFormat df = new SimpleDateFormat(ISO_8601);
+            df.setTimeZone(TimeZone.getDefault());
+            final String json = String.format(JSON_TEMPLATE, data.hostname, df.format(data.startup));
             writer.println( json );
         } else {
             resp.setContentType("text/plain");
             writer.println("Hostname:" + data.hostname);
-            writer.println("Startup: "+data.startup);
+            writer.println("Startup: "+ data.startup);
         }
         resp.flushBuffer();
     }
@@ -57,11 +64,11 @@ public class PingServlet extends HttpServlet {
 
     private class Data {
         final String hostname;
-        final long startup;
+        final Date startup;
 
         public Data(String hostname, long startup) {
             this.hostname = hostname;
-            this.startup = startup;
+            this.startup = new Date();
         }
     }
     
